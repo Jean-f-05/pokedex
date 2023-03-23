7fc03ca3f619be1d8bc4e7b6c1028ed25d961da9
Jean-f-05 <jean_f_05@hotmail.com>
2023-03-23 13:25:23 +0000

    feat: added Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import * as S from './styles';
+/* import { PokeBall } from '../../assets/svg/PokeBall' */
+const Navbar = ({ children }) => {
+    return (
+        <S.Wrapper>
+            <S.ListWrapper>
+                {/*      <PokeBall /> */}
+
+                <S.Navheader>POKEDEX</S.Navheader>
+                <div></div>
+            </S.ListWrapper>
+        </S.Wrapper>
+    );
+};
+
+export default Navbar;
+
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.nav`
+${({ theme }) => css`
+    background-color: ${theme.colors.pokemonRed};
+    height: 10vh;
+    width: 100 %;
+    color:#FCE2DB
+`}
+`;
+
+export const ListWrapper = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content:center;
+
+`;
+
+export const Navheader = styled.h1`
+${({ theme }) => css`
+    font-size: ${theme.fontSizes.large};
+`}`;
+
+
