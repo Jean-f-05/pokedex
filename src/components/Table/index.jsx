402c0e3ffbf0b248902307e42ee3b0cd45eca36a
Jean-f-05 <jean_f_05@hotmail.com>
2023-03-24 13:51:54 +0000

    feat: added axios and useAxios hook

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,18 +1,25 @@
 import * as S from './styles';
-import { pokemons } from "./mock"
+/* import { pokemons } from "./mock"; */
 import Card from '../Card';
+import { useAxios } from './useAxios';
 
 const Table = () => {
+    const { data, loading, error } = useAxios('https://pokeapi.co/api/v2/pokemon/', { limit: 151 });
+    const Loader = () => { return <h1>LOADING</h1> }
+    const Error = () => { return <h1>ERROR</h1> }
 
     return (
         <S.Wrapper>
-            {pokemons.map((pokemon) => (
-                <Card
-                    pokeUrl={pokemon.url}
-                    pokeName={pokemon.name}
-                    pokeColor={pokemon.color}
-                />
-            ))}
+            {loading ? <Loader /> :
+                (error ? <Error /> :
+                    data && data.map(pokemon => (
+                        <Card
+                            pokeUrl={pokemon.url}
+                            pokeName={pokemon.name}
+                            pokeColor={pokemon.color}
+                        />
+                    )))
+            }
         </S.Wrapper >
     );
 };
---
+++ src/components/Table/useAxios.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+export const useAxios = (customUrl, queryParams) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        const getData = async () => {
+            try {
+                const response = await axios.get(customUrl, { params: queryParams });
+                setData(response.data.results);
+            } catch (error) {
+                setError(error)
+
+            } finally {
+                setLoading(false);
+            }
+        };
+        getData();
+    }, [loading, customUrl, queryParams]);
+
+    return { data, loading, error }
+};
+
+
