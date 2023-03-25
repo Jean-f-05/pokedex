05b5da45e4c17323c2f0c18249ecfc9d9072a73c
Jean-f-05 <jean_f_05@hotmail.com>
2023-03-25 19:39:17 +0000

    feat: added filter to Table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,22 +3,32 @@ import Card from '../Card';
 import { useAxios } from './useAxios';
 import Loader from "../shared/Loader";
 
-const Table = () => {
-    const { data, loading, error } = useAxios('https://pokeapi.co/api/v2/pokemon/', { limit: 1008 });
-    const Error = () => { return <h1>ERROR</h1> }
+/* maxPokemons= 1008 */
 
+const Table = () => {
+    let { data, loading, error } = useAxios('https://pokeapi.co/api/v2/pokemonss/', { limit: 90 });
+    let filteredData;
+    const Error = () => { return <h1>ERROR</h1> }
+    const pokeName = "";
+
+    if (data && pokeName) {
+        filteredData = data.filter(pokemon => pokemon.name === pokeName)
+        data = filteredData;
+    }
     return (
         <S.Wrapper>
             {loading ? <Loader /> :
                 (error ? <Error /> :
-                    data && data.map(pokemon => (
-                        <Card
-                            key={pokemon.name}
-                            pokeUrl={pokemon.url}
-                            pokeName={pokemon.name}
-                            pokeColor={pokemon.color}
-                        />
-                    )))
+                    (!data || data.length === 0 ? <h1>Sorry, can't find that pokemon...</h1> :
+                        data && data.map(pokemon => (
+                            <Card
+                                key={pokemon.name}
+                                pokeUrl={pokemon.url}
+                                pokeName={pokemon.name}
+                                pokeColor={pokemon.color}
+                            />
+                        )))
+                )
             }
         </S.Wrapper >
     );
