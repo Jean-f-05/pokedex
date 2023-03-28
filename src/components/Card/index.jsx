9976b9d247b1a0f84945cad496dd17795c0ab0a1
Jean-f-05 <jean_f_05@hotmail.com>
2023-03-28 10:13:21 +0100

    refactor: added Card setModal state

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,16 +2,17 @@ import * as S from './styles';
 import { getId } from '../../utils';
 
 
-const Card = ({ pokeUrl, pokeName, pokeColor }) => {
+const Card = ({ pokeUrl, pokeName, pokeColor, setModal }) => {
     const id = getId(pokeUrl);
+
 
     return (
         <S.CardElement key={id} pokeColor={pokeColor}>
             <S.CardImage src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={pokeName} />
             <S.CardNumber></S.CardNumber>
             <S.CardName>{pokeName}</S.CardName>
-            <S.AnchorWrapper>
-                <S.Anchor href="/">Know more</S.Anchor>
+            <S.AnchorWrapper onClick={setModal}>
+                <S.Anchor href="#">Know more</S.Anchor>
                 <S.ChevronRight size={32} color="#3B4CCA" />
             </S.AnchorWrapper>
         </S.CardElement>
