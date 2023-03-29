2358fb42f32c92ecd6e25a4d05e4e65ae8e7b4ce
Jean-f-05 <jean_f_05@hotmail.com>
2023-03-29 20:47:12 +0100

    fix: getId util function + stdNumebr function

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,19 +1,31 @@
 export const getId = (pokeUrl) => {
     switch (pokeUrl.length) {
-        case 36:
+        case 44:
             return pokeUrl.slice(-2, -1);
 
-        case 37:
+        case 45:
             return pokeUrl.slice(-3, -1);
 
-        case 38:
+        case 46:
             return pokeUrl.slice(-4, -1);
 
-        case 39:
+        case 47:
             return pokeUrl.slice(-5, -1);
 
-        case 40: return pokeUrl.slice(-6, -1)
+        case 48: return pokeUrl.slice(-6, -1)
         default:
             return null
+    }
+}
+
+export const stdNumber = (pokeNumber) => {
+    switch (pokeNumber.length) {
+        case 1:
+            return "00" + pokeNumber;
+        case 2:
+            return "0" + pokeNumber;
+
+        default:
+            return pokeNumber
     }
 }
