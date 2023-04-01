bffedda322fea4e7f015f20752c63c571f0745b3
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-01 14:33:42 +0100

    feat: + borderColors object to style card borders

---
+++ src/components/CardDetails/borderColors.js
@@ -0,0 +1,61 @@
+import { css } from 'styled-components';
+
+export const borderColors = {
+    normal: () => css`
+    borderColor:  #A8A77A;
+    `,
+    fighting: () => css`
+    border-color:  #C22E28;
+    `,
+    flying: () => css`
+    border-color:  #A98FF3;
+    `,
+    poison: () => css`
+    border-color:  #A33EA1;
+    `,
+    ground: () => css`
+    border-color:  #E2BF65;
+    `,
+    rock: () => css`
+    border-color:  #B6A136;
+    `,
+    bug: () => css`
+    border-color:  #A6B91A;
+    `,
+    ghost: () => css`
+    border-color:  #735797;
+    `,
+    steel: () => css`
+    border-color:  #B7B7CE;
+    `,
+    fire: () => css`
+    border-color:  #EE8130;
+    `,
+    water: () => css`
+    border-color:  #6390F0;
+    `,
+    grass: () => css`
+    border-color:  #7AC74C;
+    `,
+    electric: () => css`
+    border-color:  #F7D02C;
+    `,
+    psychic: () => css`
+    border-color:  #F95587;
+    `,
+    ice: () => css`
+    border-color:  #96D9D6;
+    `,
+    dragon: () => css`
+    border-color:  #6F35FC;
+    `,
+    dark: () => css`
+    border-color:  #705746;
+    `,
+    fairy: () => css`
+    border-color:  #D685AD;
+    `,
+    unknown: () => css`
+    border-color:  #000;
+    `,
+};
