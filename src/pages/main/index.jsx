4da0dab2c8778dc4265f0ea4f737f563a6ff25f0
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-01 18:26:29 +0100

    refactor: + sortByNumber to Main Component

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,12 +3,21 @@ import SearchBar from '../../components/SearchBar';
 import Table from '../../components/Table';
 import { useAxios } from '../../components/Hooks/useAxios';
 import { createContext, useState } from 'react';
+import { sortByNumber } from "../../utils/index"
 export const PokemonsContext = createContext({});
 
 const Main = () => {
     let { request } = useAxios('https://pokeapi.co/api/v2/generation/1');
     if (request.data) {
         request.data = [request.data.pokemon_species];
+
+
+        //SORT BY TEXT
+        //request.data[0].sort((a, b) => a.name.localeCompare(b.name)); 
+
+        //SORT BY NUMBER
+        sortByNumber(request.data[0]);
+        /* request.data[0].sort((a, b) => getId(a.url) - getId(b.url)); */
     }
 
     const [pokemonName, setPokemonName] = useState("");
