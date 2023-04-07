4c5d28026971c8fda8a3f99083ea2070765eb2a9
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-07 13:18:22 +0100

    feat: + generations.js file

---
+++ src/pages/main/generations.js
@@ -0,0 +1,11 @@
+export const generations = [
+    { name: "Kanto", value: "I" },
+    { name: "Johto", value: "II" },
+    { name: "Hoenn", value: "III" },
+    { name: "Sinnoh", value: "IV" },
+    { name: "Unova", value: "V" },
+    { name: "Kalos", value: "VI" },
+    { name: "Alola", value: "VII" },
+    { name: "Galar", value: "VIII" },
+    { name: "Paldea", value: "IX" }
+];
