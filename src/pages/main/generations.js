1e9371acb46cd68e667b5599b43efdcb3e3df531
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-09 22:22:53 +0100

    fix: generations name now contains fullname

--- src/pages/main/generations.js
+++ src/pages/main/generations.js
@@ -1,11 +1,11 @@
 export const generations = [
-    { name: "Kanto", value: "I" },
-    { name: "Johto", value: "II" },
-    { name: "Hoenn", value: "III" },
-    { name: "Sinnoh", value: "IV" },
-    { name: "Unova", value: "V" },
-    { name: "Kalos", value: "VI" },
-    { name: "Alola", value: "VII" },
-    { name: "Galar", value: "VIII" },
-    { name: "Paldea", value: "IX" }
+    { name: "Gen I (KANTO)", value: "I" },
+    { name: "Gen II (JOHTO)", value: "II" },
+    { name: "Gen III (HOENN)", value: "III" },
+    { name: "Gen IV (SINNOH)", value: "IV" },
+    { name: "Gen V (UNOVA)", value: "V" },
+    { name: "Gen VI (KALOS)", value: "VI" },
+    { name: "Gen VII (ALOLA)", value: "VII" },
+    { name: "Gen VIII (GALAR)", value: "VIII" },
+    { name: "Gen IX (PALDEA)", value: "IX" }
 ];
