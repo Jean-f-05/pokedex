816c63f7097d42f632bd6e9c462d0f3c7b6df5cf
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-09 22:28:02 +0100

    IMP: attempt on fix useEffect running twice

--- src/components/Hooks/useAxios.jsx
+++ src/components/Hooks/useAxios.jsx
@@ -3,22 +3,29 @@ import axios from 'axios';
 
 
 export const useAxios = (customUrl, queryParams) => {
+    /* const dataFetchedRef = useRef(false); */
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        /*  if (dataFetchedRef.current) return; */
+
         const getData = async () => {
             try {
                 const response = await axios.get(customUrl, { params: queryParams });
                 setData(response.data);
+                /* setData(prevState => ({ ...prevState, ...response.data })); */
+                console.log("RESPONSEDATA", data)
             } catch (error) {
-                setError(error)
+                setError(error);
             } finally {
                 setLoading(false);
             }
         };
+        console.log("YUPICO!")
         getData();
+        /* dataFetchedRef.current = true; */
         //eslint-disable-next-line
     }, [customUrl]);
 
