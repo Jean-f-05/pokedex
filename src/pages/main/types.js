cb94614aca9f286be1a055082cc38e34e6a3fd6a
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-09 22:23:47 +0100

    feat: + types

---
+++ src/pages/main/types.js
@@ -0,0 +1,23 @@
+export const types = [
+    { name: "normal", value: "I" },
+    { name: "fighting", value: "II" },
+    { name: "flying", value: "III" },
+    { name: "poison", value: "IV" },
+    { name: "ground", value: "V" },
+    { name: "rock", value: "VI" },
+    { name: "bug", value: "VII" },
+    { name: "ghost", value: "VIII" },
+    { name: "fire", value: "IX" },
+    { name: "water", value: "IX" },
+    { name: "grass", value: "IX" },
+    { name: "electric", value: "IX" },
+    { name: "psychic", value: "IX" },
+    { name: "ice", value: "IX" },
+    { name: "dragon", value: "IX" },
+    { name: "dark", value: "IX" },
+    { name: "fairy", value: "IX" },
+    { name: "unknown", value: "IX" },
+];
+
+
+
