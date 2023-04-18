e93d0304e328d189eb4fb18257c30e9a667a3eb2
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-18 19:01:50 +0100

    fix: setField updates object & input dynamic name

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -2,36 +2,35 @@ import { useState } from 'react';
 import * as S from "./styles";
 import { PlusOutline, MinusOutline } from "@styled-icons/evaicons-outline";
 
-const FilterEl = ({ setValue, radio }) => {
+const FilterEl = ({ setField, radio, name }) => {
     const handleFormChange = (evt) => {
-        setValue(evt.target.value)
+        setField({ id: evt.target.value, name: evt.target.name })
     }
-
     return (
         <>
             < S.FilterRadioWrapper >
                 {radio &&
-                    radio.map((gen, i) => {
+                    radio.map((el, i) => {
                         return (
-                            <S.FilterInput key={`${gen.name}`}>
-                                <S.Label htmlFor={gen.name}>{gen.name.toUpperCase()}</S.Label>
-                                <input defaultChecked={i === 0} type="radio" id={gen.name} name="generation" value={`${i + 1}`} onChange={handleFormChange} />
+                            <S.FilterInput key={`${el.name}`}>
+                                <S.Label htmlFor={el.name}>{el.name.toUpperCase()}</S.Label>
+                                <input defaultChecked={el.name === "Gen I (KANTO)"} type="radio" id={el.name} name={name} value={el.value} onChange={handleFormChange} />
                             </S.FilterInput>)
                     })
                 }
             </S.FilterRadioWrapper >
         </>)
-
 };
 
 const Filter = ({ value, text, radio }) => {
-    const handleClick = () => {
+    const toggleFilter = () => {
         return setIsOpen(!isOpen)
     }
+
     const [isOpen, setIsOpen] = useState(false);
     return (
         <S.FilterWrapper>
-            <S.FilterHeader onClick={handleClick}>
+            <S.FilterHeader onClick={toggleFilter}>
                 <S.FilterText>
                     {text}
                 </S.FilterText>
@@ -39,10 +38,9 @@ const Filter = ({ value, text, radio }) => {
                     <MinusOutline size={24} color="white" /> :
                     <PlusOutline size={24} color="white" />}
             </S.FilterHeader>
-            {<S.FilterBody active={isOpen}>
-                <FilterEl setValue={value} radio={radio} />
+            {<S.FilterBody active={isOpen} >
+                <FilterEl setField={value} radio={radio} name={text} />
             </S.FilterBody>}
-
         </S.FilterWrapper >
     )
 };
