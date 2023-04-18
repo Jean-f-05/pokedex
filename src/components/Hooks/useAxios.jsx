1ed34f9113b0e853c317f237bc710bf62aea6615
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-18 19:03:13 +0100

    fix: useAxios returns types or generations

--- src/components/Hooks/useAxios.jsx
+++ src/components/Hooks/useAxios.jsx
@@ -1,35 +1,66 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
-export const useAxios = (customUrl, queryParams) => {
-    /* const dataFetchedRef = useRef(false); */
-    const [data, setData] = useState();
+export const useAxios = (customUrl) => {
+    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        /*  if (dataFetchedRef.current) return; */
-
         const getData = async () => {
             try {
-                const response = await axios.get(customUrl, { params: queryParams });
-                setData(response.data);
-                /* setData(prevState => ({ ...prevState, ...response.data })); */
-                console.log("RESPONSEDATA", data)
+                setLoading(true);
+                const response = await axios.get(customUrl);
+                if (response.data.pokemon_species) {
+                    setData(response.data.pokemon_species)
+                }
+                if (response.data.pokemon) {
+                    const newPokemons = response.data.pokemon.map(pokemon => ({
+                        name: pokemon.pokemon.name,
+                        url: pokemon.pokemon.url
+                    }))
+                    setData(newPokemons)
+                }
             } catch (error) {
+                console.log(error)
                 setError(error);
+                return
             } finally {
                 setLoading(false);
             }
         };
-        console.log("YUPICO!")
         getData();
-        /* dataFetchedRef.current = true; */
-        //eslint-disable-next-line
+
     }, [customUrl]);
 
     return { request: { data, loading, error } }
 };
 
 
+export const useAxiosII = (customUrl) => {
+    const [dataII, setData] = useState(null);
+    const [loadingII, setLoading] = useState(true);
+    const [errorII, setError] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setLoading(true);
+                const response = await axios.get(customUrl);
+                /* setData(response.data); */
+                console.log("RESPONSE", response)
+                setData(prevState => ({ ...prevState, ...response.data }));
+            } catch (error) {
+                console.log(error)
+                setError(error);
+                return
+            } finally {
+                setLoading(false);
+            }
+        };
+        getData();
+
+    }, [customUrl]);
+
+    return { requestII: { dataII, loadingII, errorII } }
+};
