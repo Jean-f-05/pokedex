dd1f3e44e3b494d525da80407af6bbf6f7722957
Jean-f-05 <jean_f_05@hotmail.com>
2023-04-21 15:17:18 +0100

    fix: Table backgorund width + svg

--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -1,12 +1,14 @@
 import styled, { css } from 'styled-components';
+import backgroundImg from "../../assets/svg/backgroundImg.svg"
+
 
 export const Wrapper = styled.div`
     ${({ theme }) => css`
         margin-top: 3rem ;
-        width: 95%;
-        background-color: ${theme.colors.pokemonWhite};
-        border: 0.3rem solid ${theme.colors.pokemonRed};
-        border-radius: 0.5rem;
+        width: 100%;
+        background: url(${backgroundImg});  
+        border-top: 0.3rem solid ${theme.colors.pokemonRed};
+        border-bottom: 0.3rem solid ${theme.colors.pokemonRed};
         padding: 2rem;
         display: flex;  
         flex-wrap: wrap;
@@ -14,7 +16,7 @@ export const Wrapper = styled.div`
         row-gap: 1.5rem;
         counter-reset: number;
         position: relative;
-       /*  z-index: 50; */
+       
 `}`;
 
 
